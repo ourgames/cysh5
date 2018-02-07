8df42712812da48b2cd71486d6bdd2bef5df0a92
yutao <441066277@qq.com>
2018-02-08 01:02:27 +0800

    no message

---
+++ assets/Script/RoleCtrl.js
@@ -0,0 +1,49 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+    editor: {
+        requireComponent: sp.Skeleton
+    },
+    properties: {
+        // role: cc.Node,
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        var spine = this.spine = this.getComponent('sp.Skeleton');
+        this.spine.setAnimation(0, 'working', true);
+        // var node = this.node = this.getComponent('cc.Node');
+        this.node.setPosition(0,0);
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
