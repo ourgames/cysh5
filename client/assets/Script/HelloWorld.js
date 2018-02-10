8e74848eb7a41484cbc9182fdf29ec9ed1c2c5d6
yutao <441066277@qq.com>
2018-02-10 09:36:48 +0800

    no message

--- client/assets/Script/HelloWorld.js
+++ client/assets/Script/HelloWorld.js
@@ -10,13 +10,28 @@ cc.Class({
         text: 'Hello, World!'
     },
 
+    uuidv4: function() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => 
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16) )
+    },
+
     // use this for initialization
-    onLoad: function () {
-        // this.label.string = this.text;
+    onLoad: function() {
+        var uuid = cc.sys.localStorage.getItem('uuid');
+        // this.label.string = "1";//score == null ? 0 : score;
+        if (uuid == null) {
+            uuid = this.uuidv4();
+        }
+        cc.log("--------------" + uuid)
+        cc.sys.localStorage.setItem('uuid', uuid); 
+        var la = this.getComponent(cc.Label);
+        la.string = "" + uuid;
     },
 
     // called every frame
-    update: function (dt) {
+    update: function(dt) {
 
     },
+
+
 });
